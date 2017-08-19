6ed5e3e50fa492a9ccf4c0476a55ef77bb20ce41
rkiang <ryanlkiang@gmail.com>
2017-08-19 15:56:06 -0500

    added function for monthly salary, added to HTML and added additional id's

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,14 +5,7 @@ app.controller('EmployeeController', ['$http', function ($http) {
     console.log('Employee Controller has been loaded');
     var self = this;
     self.employees = [];
-    // function monthlySalary() {
-    //     for (var i = 0; i < self.employees.length; i++) {
-    //         var employees = self.employees[i];
-    //         return self.employees.annual_salary;
-    //     }
-    // }
-    // console.log(monthlySalary());
-    
+
 
     // GET from server side
     self.getEmployees = function () {
@@ -23,6 +16,16 @@ app.controller('EmployeeController', ['$http', function ($http) {
             console.log(response);
             console.log(response.data);
             self.employees = response.data;
+
+            // Takes each employees annual salary and adds them together, then returns divided by 12(months)
+            var monthlySalary = function () {
+                self.sum = 0;
+                for (var i = 0; i < self.employees.length; i++) {
+                    self.sum = self.sum + self.employees[i].annual_salary / 12;
+                }
+                return self.sum;
+            }
+            // console.log(monthlySalary());
         });
     }
 
@@ -32,12 +35,12 @@ app.controller('EmployeeController', ['$http', function ($http) {
             method: 'POST',
             url: '/employee',
             data: self.newEmployee
-        }).then(function(response){
+        }).then(function (response) {
             console.log(response);
             self.getEmployees();
             self.newEmployee = {}; // clears the input field after a submission
-        })  
-      };
+        })
+    };
     self.getEmployees();
 
 }])
